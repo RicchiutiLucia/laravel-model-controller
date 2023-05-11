de40548cab73f0897daefbf4bc3a265ac4c0141d
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-05-11 15:14:47 +0200

    esercizio con card completato

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,12 +6,12 @@
         <div class="row">
             @foreach ($table as $elem)
                     <div class="col-3 g-3">
-                        <div class="card bg-primary h-100 " style="width: 20rem;">
+                        <div class="card bg-secondary h-100 " style="width: 20rem;">
                             <div class="card-body">
-                                <h5 class="card-title">{{$elem['title']}}</h5>
-                                <p class="card-text ">Nationality: {{$elem['nationality']}}</p>
-                                <p class="card-text">Date: {{$elem['date']}}</p>
-                                <p class="card-text">Vote: {{$elem['vote']}}</p>
+                                <h5 class="card-title text-white">{{$elem['title']}}</h5>
+                                <p class="card-text text-info ">Nationality: {{$elem['nationality']}}</p>
+                                <p class="card-text text-info">Date: {{$elem['date']}}</p>
+                                <p class="card-text text-info">Vote: {{$elem['vote']}}</p>
                             </div>
                         </div>
                     </div>

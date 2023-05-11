243a6b8b2df7d5677c395ffc6fdb939b783ec247
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-05-11 15:23:26 +0200

    aggiunto un po di stile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,9 +7,10 @@
             @foreach ($table as $elem)
                     <div class="col-3 g-3 py-3">
                         <div class="card bg-secondary h-100 w-80 " style="width: 20rem;">
-                            <img src="{{url('https://picsum.photos/300/300')}}" class="card-img-top" alt="...">
+                            <img src="{{url('https://picsum.photos/300/300')}}" class="card-img-top h-50" >
                             <div class="card-body">
-                                <h5 class="card-title text-white">{{$elem['title']}}</h5>
+                                <h5 class="card-title ">{{$elem['title']}}</h5>
+                                <p class="card-text text-info ">Original Title: {{$elem['original_title']}}</p>
                                 <p class="card-text text-info ">Nationality: {{$elem['nationality']}}</p>
                                 <p class="card-text text-info">Date: {{$elem['date']}}</p>
                                 <p class="card-text text-info">Vote: {{$elem['vote']}}</p>

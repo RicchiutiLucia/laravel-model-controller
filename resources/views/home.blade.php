77bc35e8a27cf81e37ec9055e6d54d283bde4e2d
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-05-11 15:19:23 +0200

    aggiunte immagini casuali nella card. Esercizio concluso

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,9 @@
     <div class="container">
         <div class="row">
             @foreach ($table as $elem)
-                    <div class="col-3 g-3">
-                        <div class="card bg-secondary h-100 " style="width: 20rem;">
+                    <div class="col-3 g-3 py-3">
+                        <div class="card bg-secondary h-100 w-80 " style="width: 20rem;">
+                            <img src="{{url('https://picsum.photos/300/300')}}" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title text-white">{{$elem['title']}}</h5>
                                 <p class="card-text text-info ">Nationality: {{$elem['nationality']}}</p>

8d1e099fed28f7030ffc5cf52b0c62202b0dbdf7
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-05-11 15:06:57 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+@section('content')
+    <h1 class="text-center">Movies</h1>
+    {{-- insert movies from db --}}
+    <div class="container">
+        <div class="row">
+            @foreach ($table as $elem)
+                    <div class="col-3 g-3">
+                        <div class="card bg-primary h-100 " style="width: 20rem;">
+                            <div class="card-body">
+                                <h5 class="card-title">{{$elem['title']}}</h5>
+                                <p class="card-text ">Nationality: {{$elem['nationality']}}</p>
+                                <p class="card-text">Date: {{$elem['date']}}</p>
+                                <p class="card-text">Date: {{$elem['vote']}}</p>
+                            </div>
+                        </div>
+                    </div>
+            @endforeach
+        </div>
+
+    </div>
+@endsection
